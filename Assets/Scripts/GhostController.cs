698164a0b26ec47cbe9e2e0a832dc5cc718cb138
TraunerL <ludwig.trauner@igcv.fraunhofer.de>
2022-07-20 16:15:17 +0200

    merged ghost into main

---
+++ Assets/Scripts/GhostController.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostController : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    public float horizontalCameraSensitivity = 20;
+    public float verticalCameraSensitivity = 20;
+
+    float mouseX = 0;
+    float mouseY = 0;
+
+    float horizontal;
+    float vertical;
+
+    float create_portal_pressed = 0;
+    bool portal_wall_exists = false;
+    Vector3 portal_wall_origin = new Vector3(-100, -100, -100);
+
+    private void Update()
+    {
+        if(create_portal_pressed < 1)
+        {
+            float RotationX = horizontalCameraSensitivity * mouseX * Time.deltaTime;
+            float RotationY = verticalCameraSensitivity * mouseY * Time.deltaTime;
+
+            Vector3 CameraRotation = transform.rotation.eulerAngles;
+            CameraRotation.x -= RotationY;
+            CameraRotation.y += RotationX;
+            transform.rotation = Quaternion.Euler(CameraRotation);
+
+            Vector3 moveDirection = transform.forward * vertical + transform.right * horizontal;
+            transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+            GameObject.FindWithTag("PortalWall").transform.position = portal_wall_origin;
+            
+        }
+        else
+        {
+            if (!portal_wall_exists)
+            {
+                GameObject.FindWithTag("PortalWall").transform.position = transform.position - transform.forward;
+                GameObject.FindWithTag("PortalWall").transform.rotation = transform.rotation;
+
+                GameObject.FindWithTag("PortalCamera").transform.position = transform.position;
+                GameObject.FindWithTag("PortalCamera").transform.rotation = transform.rotation;
+                // TODO: remove wall again
+            }
+        }
+    }
+
+    public void OnLookInput(float x, float y)
+    {
+        this.mouseX = x;
+        this.mouseY = y;
+
+        Debug.Log($"GhostController: LookInput: {x}, {y}");
+    }
+
+    public void OnMoveInput(float horizontal, float vertical)
+    {
+        this.vertical = vertical;
+        this.horizontal = horizontal;
+        
+        Debug.Log($"GhostController: MoveInput: {vertical}, {horizontal}");
+    }
+
+    public void OnCreatePortalInput(float create_portal_pressed)
+    {
+        this.create_portal_pressed = create_portal_pressed;
+
+        Debug.Log($"InputController: createPortalInput: {create_portal_pressed}");
+    }
+}
