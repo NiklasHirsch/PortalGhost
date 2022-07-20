6898afbb5ea9aec60d92bf70e5d5775db939535b
Manu <manueleschneckenburger@yahoo.de>
2022-07-20 14:08:33 +0200

    bug fix movement

--- Assets/Scripts/GhostController.cs
+++ Assets/Scripts/GhostController.cs
@@ -2,21 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GhostController : PortalableObject
+public class GhostController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    private GhostMovement cameraMove;
+    public float moveSpeed = 5;
+    public float horizontalCameraSensitivity = 20;
+    public float verticalCameraSensitivity = 20;
 
-    protected override void Awake()
+    float mouseX = 0;
+    float mouseY = 0;
+
+    float horizontal;
+    float vertical;
+
+    float create_portal_pressed = 0;
+    bool portal_wall_exists = false;
+
+    private void Update()
     {
-        base.Awake();
+        if(create_portal_pressed < 1)
+        {
+            float RotationX = horizontalCameraSensitivity * mouseX * Time.deltaTime;
+            float RotationY = verticalCameraSensitivity * mouseY * Time.deltaTime;
 
-        cameraMove = GetComponent<GhostMovement>();
+            Vector3 CameraRotation = transform.rotation.eulerAngles;
+            CameraRotation.x -= RotationY;
+            CameraRotation.y += RotationX;
+            transform.rotation = Quaternion.Euler(CameraRotation);
+
+            Vector3 moveDirection = transform.forward * vertical + transform.right * horizontal;
+            transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        }
+        else
+        {
+            if (!portal_wall_exists)
+            {
+                GameObject.FindWithTag("PortalWall").transform.position = transform.position - transform.forward;
+                GameObject.FindWithTag("PortalWall").transform.rotation = transform.rotation;
+                // TODO: remove wall again
+            }
+        }
     }
 
-    public override void Warp()
+    public void OnLookInput(float x, float y)
     {
-        base.Warp();
-        cameraMove.ResetTargetRotation();
+        this.mouseX = x;
+        this.mouseY = y;
+
+        Debug.Log($"GhostController: LookInput: {x}, {y}");
+    }
+
+    public void OnMoveInput(float horizontal, float vertical)
+    {
+        this.vertical = vertical;
+        this.horizontal = horizontal;
+        
+        Debug.Log($"GhostController: MoveInput: {vertical}, {horizontal}");
+    }
+
+    public void OnCreatePortalInput(float create_portal_pressed)
+    {
+        this.create_portal_pressed = create_portal_pressed;
+
+        Debug.Log($"InputController: createPortalInput: {create_portal_pressed}");
     }
 }
