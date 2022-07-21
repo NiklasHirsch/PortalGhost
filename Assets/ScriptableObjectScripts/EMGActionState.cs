4caa44b9a5561ee95f76c8c5a81c4d14e30c5af6
Manu <manueleschneckenburger@yahoo.de>
2022-07-21 19:45:24 +0200

    no message

--- Assets/ScriptableObjectScripts/EMGActionState.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Transform Container", menuName
-= "ScriptableObjects/EMG Action State")]
-public class EMGActionState : ScriptableObject
-{
-	public bool telekinesisAktive;
-	public string currentTelekinesisAction;
-}
--- Assets/ScriptableObjectScripts/SelectedObject.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Transform Container", menuName
-= "ScriptableObjects/Selected Object")]
-public class SelectedObject : ScriptableObject
-{
-	public GameObject selectedGameObject;
-}
