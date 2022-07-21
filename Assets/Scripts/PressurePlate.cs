c3cb284a2cb368222c18725e870b9c7ef2369eef
Niklas Esch <niklas@esch-online.eu>
2022-07-21 18:30:59 +0200

    lights trigger with pressure plate

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -8,7 +8,7 @@ public class PressurePlate : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        mylight = GetComponent<Light>();
+        
     }
 
     // Update is called once per frame
