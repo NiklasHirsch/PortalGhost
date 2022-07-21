558f16274dccf5e61383d7918b71cd053f309cba
EschNiklas <niklas@esch-online.eu>
2022-07-21 18:17:36 +0200

    start puzzle light implementation

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PressurePlate : MonoBehaviour
 {
+    [SerializeField] private Light mylight;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mylight = GetComponent<Light>();
     }
 
     // Update is called once per frame
@@ -18,22 +19,20 @@ public class PressurePlate : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("zero if");
         if (other.tag == "PPKey")
         {
-            Debug.Log("first if");
             float distance = Vector3.Distance(transform.position, other.transform.position);
 
             if (distance < 0.5f)
             {
-                Debug.Log("second if");
                 Rigidbody box = other.GetComponent<Rigidbody>();
-                if (box != null)
+                
+                if (box != null && mylight != null)
                 {
                     //if (box.mass >= 10)
                     //{
-                        Debug.Log("triggered");
-                        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y*0.3f, transform.localScale.z);
+                    transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y * 0.3f, transform.localScale.z);
+                    mylight.intensity = 3;
                     //}
                 }
             }
